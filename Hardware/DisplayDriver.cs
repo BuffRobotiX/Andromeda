d71a86ceb7486db73cd773c4367d216d38322951
David F. Buff <buff.robotix@yahoo.com>
2016-01-31 04:42:07 -0800

    Added a wroking mouse and drawing rectangles.

--- Hardware/DisplayDriver.cs
+++ Hardware/DisplayDriver.cs
@@ -1,12 +1,14 @@
 ﻿using Cosmos.HAL;
 using Sys = Cosmos.System;
-namespace Display
+namespace Drivers
 {
     public class DisplayDriver
     {
 
         protected VGAScreen screen;
         private int width, height;
+        private byte[] buffer;
+        private byte[] screenBuffer;
 
         public DisplayDriver()
         {
@@ -27,16 +29,19 @@ namespace Display
             7 is gray
             8 is dark blue
             9 is bright blue
+            there's 255 of them...
             */
-            screen.Clear(1);
+            screen.Clear(0);
             width = screen.PixelWidth;
             height = screen.PixelHeight;
+
+            buffer = new byte[width * height];
+            screenBuffer = new byte[width * height];
         }
 
         public virtual void setPixel(int x, int y, int c)
         {
-            if (screen.GetPixel320x200x8((uint)x, (uint)y) != (uint)c)
-                setPixelRaw(x, y, c);
+            buffer[y * width + x] = (byte)c;
         }
 
         public virtual byte getPixel(int x, int y)
@@ -68,9 +73,43 @@ namespace Display
 
         public void setPixelRaw(int x, int y, int c)
         {
-
             screen.SetPixel320x200x8((uint)x, (uint)y, (uint)c);
 
+        }
+
+        public void reDraw() //double buffer the screen
+        {
+            int c = 0;
+            byte cl;
+            byte dl;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    //cl = (byte)screen.GetPixel320x200x8((uint)x, (uint)y);
+                    cl = screenBuffer[c]; //reading from the screen is too damn slow, this buffer represents the pixels that are on the screen and reading the values out of it is wicked fast.
+                    dl = buffer[c];
+                    if (cl > dl || cl < dl) //there's a funny story behind this line, in release mode beq tries to pop the stack too many times and it fails to build if you use !=, but it works fine this way.
+                    {
+                        screen.SetPixel320x200x8((uint)x, (uint)y, buffer[c]);
+                        screenBuffer[c] = buffer[c];
+                    }
+                    buffer[c] = 0;
+                    c++;
+                }
+            }
+        }
+
+        public void drawFilledRectangle(int x0, int y0, int x1, int y1, int color)
+        {
+            for (int i = 0; i < x1 - x0; i++)
+            {
+                for (int h = 0; h < y1 - y0; h++)
+                {
+                    setPixel((x0 + i), (y0 + h), color);
+                }
+            }
         }
     }
 }
