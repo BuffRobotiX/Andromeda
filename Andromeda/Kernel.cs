3c3307a2f94efadcdd9e320a98a960d6bf6fd9a8
David F. Buff <buff.robotix@yahoo.com>
2016-01-31 00:52:03 -0800

    Working display driver and background colors.

---
+++ Andromeda/Kernel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+using Display;
+
+namespace Andromeda
+{
+    public class Kernel : Sys.Kernel
+    {
+        DisplayDriver display;
+
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Cosmos booted successfully.");
+        }
+
+        protected override void Run()
+        {
+            display = new DisplayDriver();
+            display.init();
+        }
+    }
+}
---
+++ Hardware/DisplayDriver.cs
@@ -0,0 +1,76 @@
+﻿using Cosmos.HAL;
+using Sys = Cosmos.System;
+namespace Display
+{
+    public class DisplayDriver
+    {
+
+        protected VGAScreen screen;
+        private int width, height;
+
+        public DisplayDriver()
+        {
+            screen = new VGAScreen();
+        }
+
+        public void init()
+        {
+            screen.SetGraphicsMode(VGAScreen.ScreenSize.Size320x200, VGAScreen.ColorDepth.BitDepth8);
+            /* Colors in 8 bit color depth
+            0 is black
+            1 is blue
+            2 is green
+            3 is turquoise
+            4 is red
+            5 is purpleish pink
+            6 is dark yellow
+            7 is gray
+            8 is dark blue
+            9 is bright blue
+            */
+            screen.Clear(1);
+            width = screen.PixelWidth;
+            height = screen.PixelHeight;
+        }
+
+        public virtual void setPixel(int x, int y, int c)
+        {
+            if (screen.GetPixel320x200x8((uint)x, (uint)y) != (uint)c)
+                setPixelRaw(x, y, c);
+        }
+
+        public virtual byte getPixel(int x, int y)
+        {
+            return (byte)screen.GetPixel320x200x8((uint)x, (uint)y);
+        }
+
+        public virtual void clear()
+        {
+            clear(0);
+        }
+
+        public virtual void clear(int c)
+        {
+            screen.Clear(c);
+        }
+
+        public virtual void step() { }
+
+        public int getWidth()
+        {
+            return width;
+        }
+
+        public int getHeight()
+        {
+            return height;
+        }
+
+        public void setPixelRaw(int x, int y, int c)
+        {
+
+            screen.SetPixel320x200x8((uint)x, (uint)y, (uint)c);
+
+        }
+    }
+}
